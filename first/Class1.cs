56812341ea7243eabc45f8e598577ad53196b85e
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-04-05 00:24:58 +0100

    first C# project

---
+++ first/Class1.cs
@@ -0,0 +1,24 @@
+﻿class Hello
+{
+    static void Main(string[] args)
+    {
+        // this is a single line comment
+        System.Console.WriteLine("Hello World");
+
+        /*
+         * this is 
+         * a multi line
+         * comments
+         * you can use as many line you like.
+         */
+        System.Console.WriteLine(5.5);
+        System.Console.WriteLine(5.5 * 5 + 11);
+        System.Console.WriteLine(100 % 6);
+    }
+}
+
+/* C# Built in Data Types:
+ * Numbers; Integers and Floating point numbers
+ * Strings; all sort of strings
+ * Boolean; either true of false values.
+ */
---
+++ first/StringBasics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace first
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
